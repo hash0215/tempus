83e40e46a5d9cf6266832821ab540602fa13988a
nightjune11 <134568592+nightjune11@users.noreply.github.com>
2023-12-05 13:35:48 -0500

    123

---
+++ Tempus/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float cameraSpeed = 5.0f;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = player.transform.position - this.transform.position + new Vector3(0f,2f,0f);
+        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+        this.transform.Translate(moveVector);
+    }
+}
